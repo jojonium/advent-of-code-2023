ea365423d3b69cf8e0e4b014a4b98aac7f205094
Joseph Petitti <joseph@petitti.org>
2023-12-04 00:44:21 -0500

    Day 4 part 2

--- src/Day04.hs
+++ src/Day04.hs
@@ -3,6 +3,7 @@ module Day04 (main) where
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
 import Data.List (intersect)
+import qualified Data.Map as Map
 
 getInput :: String -> IO String
 getInput defFile = do
@@ -18,7 +19,8 @@ data Card = Card
 main :: IO ()
 main = do
   cards <- map parse . lines <$> getInput "inputs/day04.txt"
-  putStrLn $ "Part 1: " ++ show (sum (map score cards))
+  putStrLn $ "Part 1: " ++ show (sum (map score1 cards))
+  putStrLn $ "Part 2: " ++ show (part2 cards)
 
 parse :: String -> Card
 parse s = Card num w h
@@ -27,8 +29,20 @@ parse s = Card num w h
                    (a:b:_) -> (a, b)
                    _       -> error "Invalid line"
 
-score :: Card -> Int
-score (Card _ winning have)
+score1 :: Card -> Int
+score1 (Card _ winning have)
   | matches > 0 = 2 ^ (matches - 1)
   | otherwise   = 0
   where matches = length (winning `intersect` have)
+
+part2  :: [Card] -> Int
+part2 cards = score2 cards initMap (length cards)
+  where initMap = Map.fromList (map (\c -> (_id c, 1)) cards)
+
+score2 :: [Card] -> Map.Map Int Int -> Int -> Int
+score2 [] m _ = Map.foldr (+) 0 m
+score2 ((Card cid winning have):cards) m end = score2 cards m' end
+  where matches = length (winning `intersect` have)
+        copies  = m Map.! cid
+        m'      = foldl f m [(cid + 1) .. (min (cid + matches) end)]
+        f ts i  = Map.insertWith (+) i copies ts
