9649804fcb64420385641598ee4d45c12f31209b
Joseph Petitti <joseph@petitti.org>
2023-12-06 08:59:50 -0500

    Solve the smart way

--- src/Day06.hs
+++ src/Day06.hs
@@ -18,5 +18,11 @@ main = do
   putStrLn $ "Part 2: " ++ show (solve [p2Time] [p2Dist])
 
 solve :: [Int] -> [Int] -> Int
-solve ts ds = product [waysToWin t d | (t, d) <- zip ts ds]
-  where waysToWin t d = length [h | h <- [1 .. (t - 1)], h * (t - h) > d]
+solve ts ds = product
+  [waysToWin (fromIntegral t) (fromIntegral d) | (t, d) <- zip ts ds]
+  
+waysToWin :: Double -> Double -> Int
+waysToWin t d = (upper - lower) + 1
+  where lower = floor   $ (t - sqrt(t * t - 4 * d)) / 2 + 1
+        upper = ceiling $ (t + sqrt(t * t - 4 * d)) / 2 - 1
+
