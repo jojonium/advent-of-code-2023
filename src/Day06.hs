ff345d3026f33683d178c68c67f006e55be4865f
Joseph Petitti <joseph@petitti.org>
2023-12-06 00:30:13 -0500

    Some cleanup

--- src/Day06.hs
+++ src/Day06.hs
@@ -9,17 +9,14 @@ getInput defFile = do
 
 main :: IO ()
 main = do
-  input <- lines <$> getInput "inputs/day06.txt"
-  let parsed    = map (drop 1 . words) input
-      -- times     = map read $ head parsed
-      -- distances = map read $ parsed !! 1
-      p2Time    = read (concat (head parsed)) :: Int
-      p2Dist    = read (concat (parsed !! 1)) :: Int
-  -- putStrLn $ "Part 1: " ++ show (solve times distances)
+  input <- map (drop 1 . words) .  lines <$> getInput "inputs/day06.txt"
+  let times     = map read $ head input
+      distances = map read $ input !! 1
+      p2Time    = read (concat (head input))
+      p2Dist    = read (concat (input !! 1))
+  putStrLn $ "Part 1: " ++ show (solve times distances)
   putStrLn $ "Part 2: " ++ show (solve [p2Time] [p2Dist])
 
 solve :: [Int] -> [Int] -> Int
 solve ts ds = product [waysToWin t d | (t, d) <- zip ts ds]
-
-waysToWin :: Int -> Int -> Int
-waysToWin t d = length [h | h <- [1 .. (t - 1)], h * (t - h) > d]
+  where waysToWin t d = length [h | h <- [1 .. (t - 1)], h * (t - h) > d]
