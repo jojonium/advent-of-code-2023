0b2e6bbf6aa90dd52378fbb916b2be415fb1926e
Joseph Petitti <joseph@petitti.org>
2023-12-08 09:38:33 -0500

    Finish day 8

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,22 +1,28 @@
 module Day07 (main) where
 
-import Control.Arrow ((&&&), (***))
+import Control.Arrow ((&&&), (***), first)
 import qualified Data.Map as Map
 
 main :: IO ()
 main = do
-  (ds, is) <- (head &&& drop 2) . lines <$> getContents
+  (ds, is) <- first cycle . (head &&& drop 2) . lines <$> getContents
   let chart = foldr parse Map.empty is
-      dirs  = cycle ds
-  putStrLn $ "Part 1: " ++ show (travel chart dirs "AAA" 0)
+      srcs  = (map (take 3) . filter ((=='A') . (!!2))) is
+  putStrLn $ "Part 1: " ++ show (part1 chart ds (=="ZZZ") 0 "AAA")
+  putStrLn $ "Part 2: " ++ show (part2 chart ds srcs ((=='Z') . (!!2)))
 
 type Chart = Map.Map String (String, String)
 
 parse :: String -> Chart -> Chart
 parse s = Map.insert (take 3 s) ((take 3 *** take 3) ((drop 7 &&& drop 12) s))
 
-travel :: Chart -> String -> String -> Int -> Int
-travel _ _ "ZZZ" n = n
-travel chart (d:ds) cur n = travel chart ds (side (chart Map.! cur)) (n + 1)
-  where side = if d == 'L' then fst else snd
-travel _ [] _ _ = error "Ran out of instructions"
+part1 :: Chart -> String -> (String -> Bool) -> Integer -> String -> Integer
+part1 chart (d:ds) p n cur
+  | p cur = n
+  | otherwise = part1 chart ds p (n + 1) next
+  where next = (if d == 'L' then fst else snd) (chart Map.! cur)
+part1 _ [] _ _ _ = error "Ran out of instructions"
+
+part2 :: Chart -> String -> [String] -> (String -> Bool) -> Integer
+part2 chart dirs sources p = foldr1 lcm x
+  where x = map (part1 chart dirs p 0) sources
