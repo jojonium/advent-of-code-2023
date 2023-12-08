0cfd8b98d13017b3797b0c1d29612b8d76ef63cf
Joseph Petitti <joseph@petitti.org>
2023-12-08 10:27:21 -0500

    Clean up

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,6 +1,6 @@
 module Day08 (main) where
 
-import Control.Arrow ((&&&), (***), first)
+import Control.Arrow ((&&&), first)
 import qualified Data.Map as Map
 
 main :: IO ()
@@ -9,13 +9,14 @@ main = do
   let chart = foldr parse Map.empty is
       srcs  = (map (take 3) . filter ((=='A') . (!!2))) is
   putStrLn $ "Part 1: " ++ show (part1 chart ds (=="ZZZ") 0 "AAA")
-  putStrLn $ "Part 2: " ++ show (part2 chart ds srcs ((=='Z') . (!!2)))
+  putStrLn $ "Part 2: " ++ show (foldr1 lcm (map (part1 chart ds ((=='Z') . (!!2)) 0) srcs))
 
 type Chart = Map.Map String (String, String)
 
 parse :: String -> Chart -> Chart
-parse s = Map.insert (take 3 s) ((take 3 *** take 3) ((drop 7 &&& drop 12) s))
+parse s = Map.insert (take 3 s) (((take 3 . drop 7) &&& (take 3 . drop 12)) s)
 
+-- chart, directions, end predicate, iteration counter, current node
 part1 :: Chart -> String -> (String -> Bool) -> Integer -> String -> Integer
 part1 chart (d:ds) p n cur
   | p cur = n
@@ -23,6 +24,3 @@ part1 chart (d:ds) p n cur
   where next = (if d == 'L' then fst else snd) (chart Map.! cur)
 part1 _ [] _ _ _ = error "Ran out of instructions"
 
-part2 :: Chart -> String -> [String] -> (String -> Bool) -> Integer
-part2 chart dirs sources p = foldr1 lcm x
-  where x = map (part1 chart dirs p 0) sources
