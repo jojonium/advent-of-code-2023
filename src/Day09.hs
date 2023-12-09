1d929e03a702b85af456e1d72fa63275cae71367
Joseph Petitti <joseph@petitti.org>
2023-12-09 16:22:53 -0500

    Finish day 9

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,16 +1,16 @@
 module Day09 (main) where
 
-import Control.Arrow ((&&&), first)
-import qualified Data.Map as Map
-
 main :: IO ()
 main = do
   seqs <- map (map read . words) . lines <$> getContents
   putStrLn $ "Part 1: " ++ show (sum (map (last . solve) seqs))
+  putStrLn $ "Part 2: " ++ show (sum (map (head . solve) seqs))
 
 solve :: [Int] -> [Int]
 solve xs
-  | all (==0) next = head xs : xs
-  | otherwise      = xs ++ [last xs + last (solve next)]
-  where next = zipWith subtract xs (tail xs)
+  | all (==0) next = head xs : head xs : xs
+  | otherwise      = newHead : xs ++ [newEnd]
+  where next    = zipWith subtract xs (tail xs)
+        newEnd  = last xs + last (solve next)
+        newHead = head xs - head (solve next)
 
