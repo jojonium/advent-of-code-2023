f1f4a3538d5f45ae961bae8aa66d22278c8250d5
Joseph Petitti <joseph@petitti.org>
2023-12-12 11:40:52 -0500

    Clean up part 1

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,13 +1,17 @@
 module Day12 (main) where
 
 import Data.List.Split (splitOn)
-import Debug.Trace (traceShow)
+import Data.List (intercalate)
+import Control.Arrow ((***))
 
 main :: IO ()
 main = do
   input <- map words . lines <$> getContents
   let rows = map (\s -> (head s, map read (splitOn "," (s !! 1)))) input
-  putStrLn $ "Part 1: " ++ show (sum (map (uncurry solve) rows))
+  -- putStrLn $ "Part 1: " ++ show (sum (map (uncurry solve) rows))
+  let unfolded = map ((intercalate "?" . replicate 5) *** (concat . replicate 5)) rows
+  -- putStrLn $ "Part 2: " ++ show (sum (map (uncurry solve) unfolded))
+  print (sum (map (uncurry solve) unfolded))
 
 solve :: String -> [Int] -> Int
 solve ss [] 
@@ -17,9 +21,9 @@ solve [] (_:_) = 0    -- ran out of space in string
 solve ss@(s:st) xs@(x:xt)
   | length ss < x = 0      -- ran out of space in string
   | s == '.' = solve st xs -- skip this working spring
-  | s == '#' = if fits x ss
-               then solve (drop (x + 1) ss) xt -- fit this number here
-               else 0 -- doesn't fit here
+  | s == '#' = if fits x ss                    -- try fitting current number
+               then solve (drop (x + 1) ss) xt -- fits here, continue
+               else 0                          -- doesn't fit here
   | s == '?' = solve ('#':st) xs + solve st xs -- try both options for '?'
   | otherwise = error "This shouldn't happen"
 
