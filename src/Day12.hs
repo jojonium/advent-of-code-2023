ca1b61dea39bc086fd3970fa12b3f1ca056cc1ee
Joseph Petitti <joseph@petitti.org>
2023-12-12 14:26:33 -0500

    Day 12 part 2

--- src/Day12.hs
+++ src/Day12.hs
@@ -2,31 +2,51 @@ module Day12 (main) where
 
 import Data.List.Split (splitOn)
 import Data.List (intercalate)
+import Data.Function (fix)
 import Control.Arrow ((***))
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
   input <- map words . lines <$> getContents
   let rows = map (\s -> (head s, map read (splitOn "," (s !! 1)))) input
-  -- putStrLn $ "Part 1: " ++ show (sum (map (uncurry solve) rows))
-  let unfolded = map ((intercalate "?" . replicate 5) *** (concat . replicate 5)) rows
-  -- putStrLn $ "Part 2: " ++ show (sum (map (uncurry solve) unfolded))
-  print (sum (map (uncurry solve) unfolded))
+      unfolded = map ((intercalate "?" . replicate 5) *** (concat . replicate 5)) rows
+  putStrLn $ "Part 2: " ++ show (sum $ map (fst . uncurry (fix (memoize . solve) Map.empty)) rows)
+  putStrLn $ "Part 2: " ++ show (sum $ map (fst . uncurry (fix (memoize . solve) Map.empty)) unfolded)
 
-solve :: String -> [Int] -> Int
-solve ss [] 
-  | '#' `elem` ss = 0 -- ran out of numbers with '#'s remaining
-  | otherwise = 1     -- reached end of numbers successfully
-solve [] (_:_) = 0    -- ran out of space in string
-solve ss@(s:st) xs@(x:xt)
-  | length ss < x = 0      -- ran out of space in string
-  | s == '.' = solve st xs -- skip this working spring
-  | s == '#' = if fits x ss                    -- try fitting current number
-               then solve (drop (x + 1) ss) xt -- fits here, continue
-               else 0                          -- doesn't fit here
-  | s == '?' = solve ('#':st) xs + solve st xs -- try both options for '?'
-  | otherwise = error "This shouldn't happen"
+type Memo = Map.Map (String, [Int]) Integer
+type SolveFunc = (Memo -> String -> [Int] -> (Integer, Memo))
 
-fits :: Int -> String -> Bool
-fits n ss = all (`elem` "#?") (take n ss) && endFits
-  where endFits = length ss == n || (`elem` ".?") (ss !! n)
+memoize :: SolveFunc -> Memo -> String -> [Int] -> (Integer, Memo)
+memoize f m ss xs = case Map.lookup (ss, xs) m of
+  Just n  -> (n, m)
+  Nothing -> let (n, m') = f m ss xs
+             in (n, Map.insert (ss, xs) n m')
+
+solve :: SolveFunc -> Memo -> String -> [Int] -> (Integer, Memo)
+solve _ m ss []
+  | '#' `elem` ss = (0, m)  -- ran out of numbers with '#'s remaining
+  | otherwise     = (1, m)  -- reached end of numbers successfully
+solve _ m [] _    = (0, m)  -- ran out of space in string
+solve f m ss@(s:st) xs
+  | length ss < (sum xs + length xs - 1) = (0, m)  -- ran out of space in string
+  | s == '.' = f m st xs
+  | s == '#' = fit f m ss xs
+  | s == '?' = let (ifBroken,  m1) = fit f m ss xs
+                   (ifWorking, m2) = f m1 st xs
+               in  (ifBroken + ifWorking, m2)
+  | otherwise = error $ "This shouldn't happen " ++ ss ++ ", " ++ show xs
+
+fit :: SolveFunc -> Memo -> String -> [Int] -> (Integer, Memo)
+fit f m ss (x:xt)
+  | all (`elem` "#?") removed = fitWorking f m remain xt
+  | otherwise = (0, m)
+  where (removed, remain) = splitAt x ss
+fit _ m _ [] = (0, m)
+
+fitWorking :: SolveFunc -> Memo -> String -> [Int] -> (Integer, Memo)
+fitWorking _ m [] [] = (1, m)
+fitWorking _ m [] _  = (0, m)
+fitWorking f m (s:st) xs
+  | s `elem` ".?" = f m st xs
+  | otherwise     = (0, m)
