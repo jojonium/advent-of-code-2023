57fb849adf6d0d572a2d836b017ec2e1e0e0563e
Joseph Petitti <joseph@petitti.org>
2023-12-13 01:36:50 -0500

    Clean up day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -6,19 +6,16 @@ import Data.List (transpose)
 main :: IO ()
 main = do
   patterns <- map lines . splitOn "\n\n" <$> getContents
-  let p1 = map (\p -> 100 * solve 0 1 (transpose p) + solve 0 1 p) patterns
-      p2 = map (\p -> 100 * solve 1 1 (transpose p) + solve 1 1 p) patterns
-  putStrLn $ "Part 1: " ++ show (sum p1)
-  putStrLn $ "Part 2: " ++ show (sum p2)
-
-mirrored :: Int -> String -> Bool
-mirrored i line = let (a, b) = splitAt i line
-                  in  all (uncurry (==)) (zip (reverse a) b)
+  let solve n p = 100 * mirrors n 1 (transpose p) + mirrors n 1 p 
+  putStrLn $ "Part 1: " ++ show (sum (map (solve 0) patterns))
+  putStrLn $ "Part 2: " ++ show (sum (map (solve 1) patterns))
 
 -- target number of non-mirrored lines -> index to split on -> lines
-solve :: Int -> Int -> [String] -> Int
-solve t n xs
+mirrors :: Int -> Int -> [String] -> Int
+mirrors t n xs
   | n >= length (head xs) = 0
   | length (filter (not . mirrored n) xs) == t = n
-  | otherwise = solve t (n + 1) xs
+  | otherwise = mirrors t (n + 1) xs
+  where mirrored i line = let (a, b) = splitAt i line
+                          in  all (uncurry (==)) (zip (reverse a) b)
 
