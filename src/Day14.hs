b4cdb2f48a534df8a15f315f2be21b39acdd18fe
Joseph Petitti <joseph@petitti.org>
2023-12-14 02:05:57 -0500

    Day 14 part 2

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,37 +1,64 @@
 module Day14 (main) where
 
-import Data.List.Split (splitOn)
-import Data.List (transpose)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+type Coord = (Int, Int)
+type Pnts  = Set.Set Coord
+data Dir   = North | South | East | West
 
 main :: IO ()
 main = do
   input <- lines <$> getContents
   let height = length input 
-      (chart, rounded, square, empty) = parse input
-  print height
-  putStrLn $ "Part 1: " ++ show (part1 chart height rounded)
+      width  = length (head input)
+      (rounded, square) = parse input
+      p1 = rollAll North square width height rounded
+  putStrLn $ "Part 1: " ++ show (sum $ map ((height -) . snd) (Set.elems p1))
+  let (a, b, r) = findRepeat (doCycle square width height) rounded
+      toRun = (1000000000 - b) `mod` (b - a)
+      p2    = iterate (doCycle square width height) r !! toRun
+  putStrLn $ "Part 2: " ++ show (sum $ map ((height -) . snd) (Set.elems p2))
 
-type Coord = (Int, Int)
-type Chart = Map.Map Coord Char
-
--- returns the chart and coords of all the Os, #s, and .s
-parse :: [String] -> (Chart, [Coord], [Coord], [Coord])
-parse ls = foldr folder (Map.empty, [], [], []) [(x, y) | x <- xs, y <- ys]
+-- Returns the coords of all square rocks and all rounded rocks
+parse :: [String] -> (Pnts, Pnts)
+parse ls = foldr folder (Set.empty, Set.empty) [(x, y) | x <- xs, y <- ys]
   where xs = [0 .. length (head ls) - 1]
         ys = [0 .. length ls - 1]
-        folder (x, y) (m, r, s, e) = (Map.insert (x, y) p m, r', s', e')
+        folder (x, y) (r, s) = (r', s')
           where p  = ls !! y !! x
-                r' = if p == 'O' then (x, y) : r else r
-                s' = if p == '#' then (x, y) : s else s
-                e' = if p == '.' then (x, y) : e else e
+                r' = if p == 'O' then Set.insert (x, y) r else r
+                s' = if p == '#' then Set.insert (x, y) s else s
 
-part1 :: Chart -> Int -> [Coord] -> Int
-part1 chart h rounded = sum $ map ((h -) . snd) finalOs
-  where finalOs = map (rollNorth chart) rounded
+rollAll :: Dir -> Pnts -> Int -> Int -> Pnts -> Pnts
+rollAll d square w h rounded = Set.map roll rounded 
+  where roll (x, y) = case d of
+          North -> let fy = if null above then y else snd (last above)
+                   in  (x, fy + os above)
+          South -> let fy = if null below then y else snd (last below)
+                   in  (x, fy - os below)
+          East  -> let fx = if null right then x else fst (last right)
+                   in  (fx - os right, y)
+          West  -> let fx = if null left  then x else fst (last left)
+                   in  (fx + os left, y)
+          where rollTo = takeWhile (`Set.notMember` square)
+                above  = rollTo [(x, y') | y' <- [y - 1, y - 2 .. 0]]
+                left   = rollTo [(x', y) | x' <- [x - 1, x - 2 .. 0]]
+                below  = rollTo [(x, y') | y' <- [y + 1 .. h - 1]]
+                right  = rollTo [(x', y) | x' <- [x + 1 .. w - 1]]
+                os cs  = length $ filter (`Set.member` rounded) cs
 
-rollNorth :: Chart -> Coord -> Coord
-rollNorth chart (x, y) = (x, fy + os)
-  where above = takeWhile ((/='#') . (chart Map.!)) [(x, y') | y' <- reverse [0 .. y - 1]]
-        os    = length $ filter ((=='O') . (chart Map.!)) above
-        fy    = if null above then y else snd (last above)
+doCycle :: Pnts -> Int -> Int -> Pnts -> Pnts
+doCycle square w h rounded = rollAll East square w h south
+  where north = rollAll North square w h rounded
+        west  = rollAll West square w h north
+        south = rollAll South square w h west
+
+-- Run until we find a repeated state, then return the indexes of the first and
+-- second time we've seen it, along with the state itself
+findRepeat :: (Pnts -> Pnts) -> Pnts -> (Int, Int, Pnts)
+findRepeat f initial = helper initial Map.empty 0
+  where helper cur prev count
+          | Just n <- Map.lookup cur prev = (n, count, cur)
+          | otherwise = helper (f cur) (Map.insert cur count prev) (count + 1)
+
