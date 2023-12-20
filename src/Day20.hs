7cb37cfe080a6b06cd4f8c591cb326127472bf91
Joseph Petitti <joseph@petitti.org>
2023-12-20 04:59:20 -0500

    Day 20 part 2

--- src/Day20.hs
+++ src/Day20.hs
@@ -6,6 +6,7 @@ import Control.Monad.State
 
 data Pulse = High | Low deriving (Eq, Show)
 type ModMap = Map.Map String Module
+type ModuleState = (ModMap, Int, Int, Map.Map String Int)
 
 data Module = 
   Broadcast [String] |
@@ -27,6 +28,10 @@ main :: IO ()
 main = do
   modMap <- initConjuncts . parse . lines <$> getContents
   putStrLn $ "Part 1: " ++ show (uncurry (*) (pushButton modMap 1000))
+  let final    = (fst . head . filter (sendsTo "rx" . snd) . Map.assocs) modMap
+      targetNs = (map fst . filter (sendsTo final . snd) . Map.assocs) modMap
+      targets  = Map.fromList (map (, 0) targetNs)
+  putStrLn $ "Part 2: " ++ show (part2 modMap targets 1)
 
 parse :: [String] -> ModMap
 parse = foldr helper Map.empty
@@ -42,33 +47,46 @@ parse = foldr helper Map.empty
 initConjuncts :: ModMap -> ModMap
 initConjuncts modMap = foldr helper modMap conjuncts
   where conjuncts = map fst (filter (isConjunct . snd) (Map.assocs modMap))
-        isConjunct (Conjunct _ _) = True
-        isConjunct _ = False
         helper cn m = Map.adjust adjFunc cn m
           where adjFunc (Conjunct mem os) = Conjunct (foldr (`Map.insert` Low) mem sources) os
                 adjFunc _ = error "Calling adjFunc on a non-conjunct module"
                 sources = map fst (filter (sendsTo cn . snd) (Map.assocs m))
+
+isConjunct :: Module -> Bool
+isConjunct (Conjunct _ _) = True
+isConjunct _ = False
 
 sendsTo :: String -> Module -> Bool
 sendsTo str (Broadcast os) = str `elem` os
 sendsTo str (FlipFlop _ os) = str `elem` os
 sendsTo str (Conjunct _ os) = str `elem` os
 
-sendPulses :: [(String, String, Pulse)] -> State (ModMap, Int, Int) (ModMap, Int, Int)
-sendPulses [] = do get
-sendPulses ((from, to, p):rest) = do
-  (modMap, lows, highs) <- get
-  let (modMap', next) = case Map.lookup to modMap of
-        Nothing -> (modMap, [])
+sendPulses :: Int -> [(String, String, Pulse)] -> State ModuleState ModuleState
+sendPulses _ [] = do get
+sendPulses bps ((from, to, p):rest) = do
+  (modMap, lows, highs, cycles) <- get
+  let (modMap', next, cycles') = case Map.lookup to modMap of
+        Nothing -> (modMap, [], cycles)
         Just aMod -> let (mod', ns) = process from p to aMod
-                     in  (Map.insert to mod' modMap, ns)
+                         cyc = if from `Map.member` cycles && p == High
+                               then Map.insert from bps cycles
+                               else cycles
+                     in  (Map.insert to mod' modMap, ns, cyc)
       lows'  = if p == Low  then lows  + 1 else lows
       highs' = if p == High then highs + 1 else highs
-  put (modMap', lows', highs')
-  sendPulses (rest ++ next)
+  put (modMap', lows', highs', cycles')
+  sendPulses bps (rest ++ next)
 
 pushButton :: ModMap -> Int -> (Int, Int)
 pushButton _ 0 = (0, 0)
 pushButton modMap n = (lows + nextLows, highs + nextHighs)
-  where (modMap', lows, highs) = evalState (sendPulses [("button", "broadcaster", Low)]) (modMap, 0, 0)
+  where initPulse = [("button", "broadcaster", Low)]
+        (modMap', lows, highs, _) = evalState (sendPulses 0 initPulse) (modMap, 0, 0, Map.empty)
         (nextLows, nextHighs)  = pushButton modMap' (n - 1)
+
+part2 :: ModMap -> Map.Map String Int -> Int -> Int
+part2 modMap cycles n = 
+  let initPulse = [("button", "broadcaster", Low)]
+      (modMap', _, _, cycles') = evalState (sendPulses n initPulse) (modMap, 0, 0, cycles)
+  in  if all (> 0) (Map.elems cycles') then foldr1 lcm (Map.elems cycles')
+      else part2 modMap' cycles' (n + 1)
