116d5bfc699e990f90ea40d1e360418a4a8aa369
Joseph Petitti <joseph@petitti.org>
2023-12-22 01:52:19 -0500

    Speed up day 22

--- src/Day22.hs
+++ src/Day22.hs
@@ -8,7 +8,7 @@ import Data.Maybe (mapMaybe)
 import qualified Data.Map as Map
 import Debug.Trace (traceShow)
 
-data Coord = Coord {_x :: Int, _y :: Int, _z :: Int} deriving (Eq, Show)
+data Coord = Coord {_x :: Int, _y :: Int, _z :: Int} deriving (Eq, Ord, Show)
 data Block = Block {_s :: Coord, _e :: Coord, _label :: String} deriving (Eq, Show)
 type BlockMap = Map.Map String [String]
 type Settled  = Map.Map Coord String
@@ -18,7 +18,7 @@ main = do
   let labels = [1..] >>= flip replicateM ['A' .. 'Z']
   blocks <- sortOn (_z . _s) . map (uncurry parse) . (`zip` labels) . lines <$> getContents
   let initMap = Map.fromList (map ((, []) . _label) blocks)
-      (supports, supportedBy, settled) = fall' initMap initMap [] blocks
+      (supports, supportedBy, settled) = fall' initMap initMap Map.empty blocks
       loneSupports    = (nub . concat . filter ((== 1) . length . nub) . map snd . Map.toList) supportedBy
       disintegratable = (filter (`notElem` loneSupports)  . map _label) blocks
   putStrLn $ "Part 1: " ++ show (length disintegratable)
@@ -32,23 +32,24 @@ pointsBelow :: Block -> [Coord]
 pointsBelow (Block (Coord sx sy sz) (Coord ex ey ez) _) =
   [Coord x y (z - 1) | x <- [sx .. ex], y <- [sy .. ey], z <- [sz .. ez]]
 
-blocksThatContain :: Block -> [Coord] -> [String]
-blocksThatContain = mapMaybe . contains
-  where contains (Block (Coord sx sy sz) (Coord ex ey ez) l) (Coord x y z)
-          | x >= sx && x <= ex && y >= sy && y <= ey && z >= sz && z <= ez = Just l
-          | otherwise = Nothing
+blocksThatContain :: Settled -> [Coord] -> [String]
+blocksThatContain settled = mapMaybe (`Map.lookup` settled)
 
 moveDown :: Block -> Block
 moveDown (Block (Coord sx sy sz) (Coord ex ey ez) l) =
   Block (Coord sx sy (sz - 1)) (Coord ex ey (ez - 1)) l
 
-fall' :: BlockMap -> BlockMap -> [Block] -> [Block] -> (BlockMap, BlockMap, [Block])
+settle :: Settled -> Block -> Settled
+settle settled (Block (Coord sx sy sz) (Coord ex ey ez) l) =
+  let coords = [Coord x y z | x <- [sx .. ex], y <- [sy .. ey], z <- [sz .. ez]]
+  in  foldr (`Map.insert` l) settled coords
+
+fall' :: BlockMap -> BlockMap -> Settled -> [Block] -> (BlockMap, BlockMap, Settled)
 fall' supports supportedBy settled [] = (supports, supportedBy, settled)
 fall' supports supportedBy settled (f:falling)
-  | traceShow (length settled) (_z (_s f)) == 1 = fall' supports supportedBy (settled ++ [f]) falling
-  | not (null blocksBelow) = fall' supports' supportedBy' (settled ++ [f]) falling
+  | _z (_s f) == 1 = fall' supports supportedBy (settle settled f) falling
+  | not (null blocksBelow) = fall' supports' supportedBy' (settle settled f) falling
   | otherwise = fall' supports supportedBy settled (moveDown f : falling)
-  where pb = pointsBelow f
-        blocksBelow  = concatMap (`blocksThatContain` pb) settled
+  where blocksBelow  = blocksThatContain settled (pointsBelow f)
         supports'    = foldr (\s m -> Map.insertWith (++) s [_label f] m) supports blocksBelow
         supportedBy' = foldr (\s m -> Map.insertWith (++) (_label f) [s] m) supportedBy blocksBelow
