887d82795481b58c7f967673ee0023a60f1f8e36
Joseph Petitti <joseph@petitti.org>
2023-12-22 02:37:14 -0500

    Day 22 part 2

--- src/Day22.hs
+++ src/Day22.hs
@@ -6,7 +6,7 @@ import Data.List (sortOn, nub)
 import Control.Monad (replicateM)
 import Data.Maybe (mapMaybe)
 import qualified Data.Map as Map
-import Debug.Trace (traceShow)
+import qualified Data.Set as Set
 
 data Coord = Coord {_x :: Int, _y :: Int, _z :: Int} deriving (Eq, Ord, Show)
 data Block = Block {_s :: Coord, _e :: Coord, _label :: String} deriving (Eq, Show)
@@ -17,11 +17,13 @@ main :: IO ()
 main = do
   let labels = [1..] >>= flip replicateM ['A' .. 'Z']
   blocks <- sortOn (_z . _s) . map (uncurry parse) . (`zip` labels) . lines <$> getContents
-  let initMap = Map.fromList (map ((, []) . _label) blocks)
-      (supports, supportedBy, settled) = fall' initMap initMap Map.empty blocks
+  let initMap         = Map.fromList (map ((, []) . _label) blocks)
+      supportedBy     = fall initMap Map.empty blocks
       loneSupports    = (nub . concat . filter ((== 1) . length . nub) . map snd . Map.toList) supportedBy
       disintegratable = (filter (`notElem` loneSupports)  . map _label) blocks
+      p2 = sum (map (part2 supportedBy . Set.singleton . _label) blocks)
   putStrLn $ "Part 1: " ++ show (length disintegratable)
+  putStrLn $ "Part 2: " ++ show p2
 
 parse :: String -> String -> Block
 parse str label = case map read (splitOneOf ",~" str) of
@@ -33,7 +35,7 @@ pointsBelow (Block (Coord sx sy sz) (Coord ex ey ez) _) =
   [Coord x y (z - 1) | x <- [sx .. ex], y <- [sy .. ey], z <- [sz .. ez]]
 
 blocksThatContain :: Settled -> [Coord] -> [String]
-blocksThatContain settled = mapMaybe (`Map.lookup` settled)
+blocksThatContain = mapMaybe . flip Map.lookup
 
 moveDown :: Block -> Block
 moveDown (Block (Coord sx sy sz) (Coord ex ey ez) l) =
@@ -44,12 +46,19 @@ settle settled (Block (Coord sx sy sz) (Coord ex ey ez) l) =
   let coords = [Coord x y z | x <- [sx .. ex], y <- [sy .. ey], z <- [sz .. ez]]
   in  foldr (`Map.insert` l) settled coords
 
-fall' :: BlockMap -> BlockMap -> Settled -> [Block] -> (BlockMap, BlockMap, Settled)
-fall' supports supportedBy settled [] = (supports, supportedBy, settled)
-fall' supports supportedBy settled (f:falling)
-  | _z (_s f) == 1 = fall' supports supportedBy (settle settled f) falling
-  | not (null blocksBelow) = fall' supports' supportedBy' (settle settled f) falling
-  | otherwise = fall' supports supportedBy settled (moveDown f : falling)
+fall :: BlockMap -> Settled -> [Block] -> BlockMap
+fall supportedBy _ [] = supportedBy
+fall supportedBy settled (f:falling)
+  | _z (_s f) == 1 = fall supportedBy (settle settled f) falling
+  | not (null blocksBelow) = fall supportedBy' (settle settled f) falling
+  | otherwise = fall supportedBy settled (moveDown f : falling)
   where blocksBelow  = blocksThatContain settled (pointsBelow f)
-        supports'    = foldr (\s m -> Map.insertWith (++) s [_label f] m) supports blocksBelow
         supportedBy' = foldr (\s m -> Map.insertWith (++) (_label f) [s] m) supportedBy blocksBelow
+
+part2 :: BlockMap -> Set.Set String -> Int
+part2 supportedBy fallen
+  | Set.size next == Set.size fallen = Set.size next - 1
+  | otherwise = part2 supportedBy next
+  where next = (Set.fromList . map fst . filter shouldFall . Map.toList) supportedBy
+        shouldFall (a, cs) = a `Set.member` fallen || (not (null cs) && all (`Set.member` fallen) cs)
+
